edc195b1df827982c834ae315da7d67797b96fa6
Gil Andrey <gil_andrey@hotmail.com>
2025-09-25 16:39:45 -0300

    Eslint

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const Pizza = (props) => {
   ]);
 };
 
-const app = () => {
+const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Padre Gino's"),
     React.createElement(Pizza, {
@@ -33,4 +33,4 @@ const app = () => {
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(React.createElement(app));
+root.render(React.createElement(App));
