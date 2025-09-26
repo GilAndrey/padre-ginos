cc6c10753d3cf9fb9794c9af7212cd1473233df5
Gil Andrey <gil_andrey@hotmail.com>
2025-09-26 16:29:43 -0300

    Adding Static Assets, API implementation

--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-
-export const Pizza = (props) => {
-    const x = document.getElementById("blah");
+const Pizza = (props) => {
     return (
         <div className="pizza">
             <h1>{props.name}</h1>
             <p>{props.description}</p>
+            <img src={props.image} alt={props.name} />
         </div>
     );
 };
+
+export default Pizza;
 
 export const Blah = () => <h1>Blah</h1>
